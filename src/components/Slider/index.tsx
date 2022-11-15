a123e1911bb39e976409dec8aca5a2d6d65567a0
artem.k@thewhitelabelagency.com <Astro2k19>
2022-11-15 16:30:06 +0200

    fix: problems with touch events on slider

--- src/components/Slider/index.tsx
+++ src/components/Slider/index.tsx
@@ -1,5 +1,5 @@
-import {SliderList} from "./SliderList";
-import {Dots} from "./Controls/Dots";
+import { SliderList } from "./SliderList";
+import { Dots } from "./Controls/Dots";
 import firstSlide from "@assets/slider/illustration.svg";
 import secondSlide from "@assets/slider/illustration2.svg";
 import thirdSlide from "@assets/slider/illustration3.svg";
@@ -7,79 +7,87 @@ import styles from "@styles/components/Slider.module.scss";
 import React from "react";
 
 export interface ISlide {
-    url: string;
-    title: string;
-    text: string;
+  url: string;
+  title: string;
+  text: string;
 }
 
 export const SliderContext = React.createContext({});
 
 export const Slider = () => {
-    const [slide, setSlide] = React.useState<number>(0);
-    const [sliderList, setSliderList] = React.useState<ISlide[]>([]);
-    const [touchStartPosition, setTouchStartPosition] = React.useState<number>(0);
+  const [slide, setSlide] = React.useState<number>(0);
+  const [sliderList, setSliderList] = React.useState<ISlide[]>([]);
+  const [touchStartPosition, setTouchStartPosition] = React.useState<
+    number | null
+  >(0);
 
-    React.useEffect(() => {
-        setSliderList([
-            {
-                url: firstSlide,
-                title: "Gain total control of your money",
-                text: "Become your own money manager and make every cent count",
-            },
-            {
-                url: secondSlide,
-                title: "Know where your money goes",
-                text: "Track your transaction easily, with categories and financial report",
-            },
-            {
-                url: thirdSlide,
-                title: "Planning ahead",
-                text: "Setup your budget for each category so you in control",
-            },
-        ]);
-    }, []);
+  React.useEffect(() => {
+    setSliderList([
+      {
+        url: firstSlide,
+        title: "Gain total control of your money",
+        text: "Become your own money manager and make every cent count",
+      },
+      {
+        url: secondSlide,
+        title: "Know where your money goes",
+        text: "Track your transaction easily, with categories and financial report",
+      },
+      {
+        url: thirdSlide,
+        title: "Planning ahead",
+        text: "Setup your budget for each category so you in control",
+      },
+    ]);
+  }, []);
 
-    const changeSlide = (direction: number) => {
-        const newSlide =
-            slide + direction < 0
-                ? sliderList.length - 1
-                : (slide + direction) % sliderList.length;
+  const changeSlide = (direction: number) => {
+    const newSlide =
+      slide + direction < 0
+        ? sliderList.length - 1
+        : (slide + direction) % sliderList.length;
 
-        setSlide(newSlide);
-    };
+    setSlide(newSlide);
+  };
 
-    const changeSpecificSlide = (index: number) => {
-        setSlide(index % sliderList.length);
-    };
+  const changeSpecificSlide = (index: number) => {
+    setSlide(index % sliderList.length);
+  };
 
-    const onTouchStart = (event: React.TouchEvent) => {
-        const touchStart = event.touches[0].pageX;
-        setTouchStartPosition(touchStart)
-    };
+  const onTouchStart = (event: React.TouchEvent) => {
+    const touchStart = event.touches[0].pageX;
+    setTouchStartPosition(touchStart);
+  };
 
-    const onTouchMove = (event: React.TouchEvent) => {
-         const touchMove = event.touches[0].pageX;
-         const difference = touchStartPosition - touchMove;
+  const onTouchMove = (event: React.TouchEvent) => {
+    if (touchStartPosition === null) return;
 
-         if (difference > 20) {
-             changeSlide(1);
-         } else if (difference < -20) {
-             changeSlide(-1);
-         }
+    const touchMove = event.touches[0].pageX;
+    const difference = touchStartPosition - touchMove;
 
-    };
+    console.log(difference, "diff");
 
+    if (difference > 5) {
+      changeSlide(1);
+    } else if (difference < -5) {
+      changeSlide(-1);
+    }
 
-    return (
-        <SliderContext.Provider value={{slide, sliderList, changeSpecificSlide}}>
-            <div
-                className={styles.slider}
-                onTouchStart={onTouchStart}
-                onTouchMove={onTouchMove}
-            >
-                <SliderList/>
-                <Dots/>
-            </div>
-        </SliderContext.Provider>
-    );
+    setTouchStartPosition(null);
+  };
+
+  return (
+    <div
+      className={styles.slider}
+      onTouchStart={onTouchStart}
+      onTouchMove={onTouchMove}
+    >
+      <SliderContext.Provider
+        value={{ slide, sliderList, changeSpecificSlide }}
+      >
+        <SliderList />
+        <Dots />
+      </SliderContext.Provider>
+    </div>
+  );
 };
